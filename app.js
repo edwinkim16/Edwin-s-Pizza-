5c780b264a0a69c566bc3f4b5bc8d9ecc9be8803
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-02 19:54:54 +0300

    getting input for first selection

--- app.js
+++ app.js
@@ -46,5 +46,33 @@ $(document).ready(function(){
         var pizzaNumberTotal = parseInt($("#pizzaNumberTotal").val());
         var deliveryLocation = $("#location").val();
 
+        $(".formOutput").show()
+        //New Customer
+        var customerOrder=new CustomerOrder //(typeOfPizza, pizzaSize, 
+            //crust, toppings, deliveryOption, pizzaNumberTotal, 
+            //deliveryLocation );
+
+            //Crispy option >onion_sausage_bacon
+            if (pizzaSize == "small" && crust == "crispy" && toppings == "onion_sausage_bacon" && deliveryOption == "on"){
+                var totalPrice = (customerOrder.edwinPizza.pizzaSizePrice[1].small + 
+                    customerOrder.edwinPizza.pizzaCrustPrice[1].crispy + 
+                    customerOrder.edwinPizza.toppingPrice[1].onion_sausage_bacon + 
+                    customerOrder.edwinPizza.deliveryPrice) * pizzaNumberTotal;
+        
+                var orderSummary = `
+                    <h2>Order Summary</h2>
+                    <p>Type of Pizza: ${typeOfPizza}</p>
+                    <p>Delivery location: ${deliveryLocation}</p>
+                    <p>Number of orders: ${pizzaNumberTotal}</p>
+                    <p>Pizza size: Small - KSh. ${customerOrder.edwinPizza.pizzaSizePrice[1].small} </p>
+                    <p>Crust: Crispy - KSh. ${customerOrder.edwinPizza.pizzaCrustPrice[1].crispy} </p>
+                    <p>Toppings: onion, Sausage and Bacon - KSh. ${customerOrder.edwinPizza.toppingPrice[1].onion_sausage_bacon}</p>
+                    <p>Delivery: - KSh. ${customerOrder.edwinPizza.deliveryPrice}</p>
+                    <hr>
+                    <p>Total: - KSh. ${totalPrice}
+                `;
+                $(".formOutput").html(orderSummary);
+                alert("your pizza will be delivered at " + deliveryLocation)
+            }
     })
 })
